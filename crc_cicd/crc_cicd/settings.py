3d0a0d315248209121348988c757ec3ddf0b68b7
kx96ba@ad <janusz.szarek@ing.com>
2021-05-07 10:29:03 +0200

    init

--- crc_cicd/crc_cicd/settings.py
+++ crc_cicd/crc_cicd/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0$kw0%f%6$wm)u#yv^9tz3hn6)tce==@i!(ip-zf(wr349i300'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
