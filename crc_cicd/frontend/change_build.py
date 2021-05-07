42fdbe11bc87a8cf598247380e0cfee26dda37e5
kx96ba@ad <janusz.szarek@ing.com>
2021-05-07 16:01:13 +0200

    init

--- crc_cicd/frontend/change_build.py
+++ crc_cicd/frontend/change_build.py
@@ -7,16 +7,14 @@ if len(sys.argv) < 3:
     raise SystemExit('Missing parameters')
 
 build_number = sys.argv[1]
-release_number = sys.argv[2]
+
 try:
     with open('package.json', 'r') as f:
         package = json.load(f)
-    package['release'] = release_number
-    package['build'] = build_number
+    package['version'] = build_number
     with open('package.json', 'w') as f:
         json.dump(package, f, indent=2)
 except FileNotFoundError:
     raise SystemExit('File package.json not found')
 
 print(f'Successful change build version to {build_number}')
-print(f'Successful change release version to {release_number}')
